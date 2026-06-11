91a3f507483cb8678cf9418ce746863d7fd25806
DUY KHANG <ntdk11n@gmail.com>
2026-06-11 15:05:40 +0700

    update

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,6 @@
+//Khai báo một đối tượng
+
+
 
 function inBCC(n){
 
@@ -63,4 +66,73 @@ function addItem(name, price, description, linkproduct, image){
     document.getElementById('container-product-list').appendChild(item);
 
 
+}
+
+
+
+const product = {
+    id:"1",
+    name:"Hoa tulip",
+    price:250000,
+    description:"Đây là giống Hoa được yêu thích ở Pháp được nhập khẩu  về Việt Nam ...",
+    image:"../assets/img/tulip.webp",
+    link:"chi-tiet.html"
+};
+
+const products = [
+    {id:"1",
+    name:"Nước hoa Montblanc",
+    price:2500000,
+    description:"Hương thơm nam tính và ấm áp, lý tưởng cho những buổi tối lãng mạn.",
+    image:"../assets/img/montblanc.jpg",
+    link:"chi-tiet.html"
+},
+    {id:"2",
+    name:"Nước hoa Versace",
+    price:3500000,
+    description:"Nước hoa Versace Bright Crystal EDT",
+    image:"../assets/img/versace.webp",
+    link:"chi-tiet.html"
+},
+    {id:"3",
+    name:"Nước hoa Dior",
+    price:3800000,
+    description:"Hương thơm nam tính và ấm áp, lý tưởng cho những buổi tối lãng mạn.",
+    image:"../assets/img/dior.jpg",
+    link:"chi-tiet.html"
+},
+    {id:"4",
+    name:"Nước hoa 9pm",
+    price:100000,
+    description:"Nước Hoa Dubai Nam Afnan 9PM EDP 100ml Chính Hãng",
+    image:"../assets/img/9pm.jpg",
+    link:"chi-tiet.html"
+}
+];
+
+function addItemV2(product){
+    document.getElementById("product-list").innerHTML += `
+        <div class="col">
+            <div class="card product-item h-100">
+                <div class="product-image">
+                    <img class="card-img-top" src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="card-body bg-light product-info">
+                    <h4 class="card-title">${product.name}</h4>
+                    <h5 class="card-text">${product.price} VNĐ</h5>
+                    <p class="card-text">${product.description}</p>
+                    <a class="btn btn-info" href="${product.link}">Xem chi tiết</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function loadAllProduct(){
+    let i=0;
+    while(i < products.length)
+    {
+        addItemV2(products[i]);
+        i++;
+    }
 }
